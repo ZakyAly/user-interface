6450e11bb0060e3cb8449df4c232ce0cb2fc0189
Zaky Aly <zaky3ly@gmail.com>
2021-01-27 21:08:19 +0100

    code refactoring

---
+++ src/components/ProductRow/ProductRow.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProductRow = (props) => {
+  const { product } = props;
+  const coloredName = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td align="left">{coloredName} </td>
+      <td align="right">{product.price} </td>
+    </tr>
+  );
+};
+
+export default ProductRow;
